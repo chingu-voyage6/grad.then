6e58576a33db4bbe5e91f3045cb5f699240cabfc
spences10 <spences10apps@gmail.com>
2017-12-13 09:05:36 +0000

    add faker

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import faker from 'faker'
 
-import { StyledH1, StyledP } from "../theme/globalStyle";
+import { StyledH1, StyledP } from "../theme/globalStyle"
 
 const IndexPage = () => (
   <div>
     <StyledH1>Hi people</StyledH1>
-    <StyledP>Welcome to your new Gatsby site.</StyledP>
+    <StyledP>{faker.lorem.paragraph()}</StyledP>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
