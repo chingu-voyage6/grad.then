a6d05d73e2bf40f80a9ce3289d8d82a282a27679
spences10 <spences10apps@gmail.com>
2017-12-15 18:49:49 +0000

    Prettier :kiss:

--- src/layouts/components/Header.js
+++ src/layouts/components/Header.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import styled from 'styled-components';
+import React from 'react'
+import Link from 'gatsby-link'
+import styled from 'styled-components'
 
-import { StyledH1, ColorPalette as CP } from "../../theme/globalStyle";
-import PageNav from "./PageNav.js";
+import { StyledH1, ColorPalette as CP } from '../../theme/globalStyle'
+import PageNav from './PageNav.js'
 
 const StyledHeader = styled.div`
   grid-area: hd;
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: auto;
-  grid-template-areas:
-    ". brand brand . . . nav nav nav nav nav .";
-`;
+  grid-template-areas: '. brand brand . . . nav nav nav nav nav .';
+`
 
 const BrandTitle = StyledH1.extend`
   grid-area: brand;
   color: ${CP.primary.light};
   font-size: 1.6em;
   padding: 0.5rem;
-`;
+`
 
 const BrandLink = styled(Link)`
   color: inherit;
-  &:visited, &:active {
-     color: inherit;
+  &:visited,
+  &:active {
+    color: inherit;
   }
-`;
+`
 
 const Header = () => (
   <StyledHeader>
@@ -35,6 +35,6 @@ const Header = () => (
     </BrandTitle>
     <PageNav header={true} />
   </StyledHeader>
-);
+)
 
-export default Header;
+export default Header
