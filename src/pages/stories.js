5126aae665c1871a557bc7fa549390e7b164f823
spences10 <spences10apps@gmail.com>
2017-12-16 14:38:39 +0000

    add card into stories page

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import { StyledH1, StyledP } from '../theme/globalStyle'
+import StoryCard from '../layouts/components/StoryCard'
 
 const StoriesPage = () => (
   <div>
     <StyledH1>Stories Page</StyledH1>
     <StyledP>This is the Stories page</StyledP>
+    <StoryCard />
   </div>
 )
 
