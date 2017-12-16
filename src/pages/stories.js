8313fa868694d0cf4abbe9cf0abf2cb4bce99465
spences10 <spences10apps@gmail.com>
2017-12-16 17:37:19 +0000

    use faker, pass props

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -3,11 +3,17 @@ import React from 'react'
 import { StyledH1, StyledP } from '../theme/globalStyle'
 import StoryCard from '../layouts/components/StoryCard'
 
+import faker from 'faker'
+
 const StoriesPage = () => (
   <div>
     <StyledH1>Stories Page</StyledH1>
     <StyledP>This is the Stories page</StyledP>
-    <StoryCard />
+    <StoryCard
+      title={faker.commerce.productName()}
+      text={faker.hacker.phrase()}
+      img={faker.image.imageUrl()}
+    />
   </div>
 )
 
