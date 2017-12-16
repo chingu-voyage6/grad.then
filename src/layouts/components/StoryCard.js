489de209bf39ab99a262370759fa79e80dad6136
spences10 <spences10apps@gmail.com>
2017-12-16 17:37:46 +0000

    use styled.img not div :upside_down_face:

--- src/layouts/components/StoryCard.js
+++ src/layouts/components/StoryCard.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import faker from 'faker'
-
 import { StyledH3, StyledP, ColorPalette as CP } from '../../theme/globalStyle'
-
-const img = faker.image.imageUrl()
 
 const Wrapper = styled.div`
   width: 400px;
@@ -20,43 +16,57 @@ const Wrapper = styled.div`
 const StoryTitle = StyledH3.extend`
   color: ${CP.text};
   margin: 0.05rem;
-  padding: 0.05rem;
+  padding: 0rem;
 `
 
 const StoryText = StyledP.extend`
   color: ${CP.text};
   margin: 0.05rem;
-  padding: 0.05rem;
+  padding: 0rem;
 `
 
 const Text = styled.div`
   grid-area: text;
   padding: 0.5rem;
-  margin: 0.5rem;
+  margin: 0rem;
   color: ${CP.text};
 `
 
-const Image = styled.div`
+// const Image = styled.div`
+//   grid-area: img;
+//   justify-self: center;
+//   padding: 1.75rem;
+//   margin: 1.3rem 0.5rem 0.5rem 0.5rem;
+//   border-radius: 90px;
+//   border: 1px solid #000;
+//   background-image: url(${img});
+//   background-size: 100px;
+//   width: 40px;
+//   height: 40px;
+// `
+
+const Image = styled.img`
   grid-area: img;
   justify-self: center;
-  padding: 1.75rem;
   margin: 1.3rem 0.5rem 0.5rem 0.5rem;
   border-radius: 90px;
   border: 1px solid #000;
-  background-image: url(${img});
-  background-size: 100px;
-  width: 40px;
-  height: 40px;
+  width: 100px;
+  height: 100px;
 `
 
-export const StoryCard = () => (
-  <Wrapper>
-    <Image />
-    <Text>
-      <StoryTitle>{faker.commerce.productName()}</StoryTitle>
-      <StoryText>{faker.lorem.paragraph()}</StoryText>
-    </Text>
-  </Wrapper>
-)
+class StoryCard extends React.Component {
+  render() {
+    return (
+      <Wrapper>
+        <Image src={this.props.img} />
+        <Text>
+          <StoryTitle>{this.props.title}</StoryTitle>
+          <StoryText>{this.props.text}</StoryText>
+        </Text>
+      </Wrapper>
+    )
+  }
+}
 
 export default StoryCard
