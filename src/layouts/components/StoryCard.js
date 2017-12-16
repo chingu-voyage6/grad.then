28d9e9ceb19532ad39e2b6d7564ab551b41d1afd
spences10 <spences10apps@gmail.com>
2017-12-16 16:27:31 +0000

    allignment done :upside_down_face:

--- src/layouts/components/StoryCard.js
+++ src/layouts/components/StoryCard.js
@@ -3,21 +3,44 @@ import styled from 'styled-components'
 
 import faker from 'faker'
 
-import { StyledH2, StyledP, ColorPalette as CP } from '../../theme/globalStyle'
+import { StyledH3, StyledP, ColorPalette as CP } from '../../theme/globalStyle'
 
 const img = faker.image.imageUrl()
 
-const Card = styled.div`
-  padding: 1.75rem;
-  margin: 0.5rem;
-  border-radius: 4px;
-  color: ${CP.text};
+const Wrapper = styled.div`
+  width: 400px;
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-template-rows: auto;
+  grid-template-areas: 'img text text';
   background: ${CP.primary.light};
+  border-radius: 4px;
+`
+
+const StoryTitle = StyledH3.extend`
+  color: ${CP.text};
+  margin: 0.05rem;
+  padding: 0.05rem;
+`
+
+const StoryText = StyledP.extend`
+  color: ${CP.text};
+  margin: 0.05rem;
+  padding: 0.05rem;
+`
+
+const Text = styled.div`
+  grid-area: text;
+  padding: 0.5rem;
+  margin: 0.5rem;
+  color: ${CP.text};
 `
 
 const Image = styled.div`
+  grid-area: img;
+  justify-self: center;
   padding: 1.75rem;
-  margin: 0.5rem;
+  margin: 1.3rem 0.5rem 0.5rem 0.5rem;
   border-radius: 90px;
   border: 1px solid #000;
   background-image: url(${img});
@@ -27,11 +50,13 @@ const Image = styled.div`
 `
 
 export const StoryCard = () => (
-  <Card>
-    <StyledH2>This is the Story Card</StyledH2>
-    <StyledP>This is the p</StyledP>
+  <Wrapper>
     <Image />
-  </Card>
+    <Text>
+      <StoryTitle>{faker.commerce.productName()}</StoryTitle>
+      <StoryText>{faker.lorem.paragraph()}</StoryText>
+    </Text>
+  </Wrapper>
 )
 
 export default StoryCard
