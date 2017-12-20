01696303434be25e67021898e12f07c992616728
spences10 <spences10apps@gmail.com>
2017-12-20 09:50:36 +0000

    add Link

--- src/layouts/components/JobsList.js
+++ src/layouts/components/JobsList.js
@@ -1,32 +1,56 @@
 import React from 'react'
 import styled from 'styled-components'
+import Link from 'gatsby-link'
 
-import { StyledH3, StyledP } from '../../theme/globalStyle'
+import { StyledH3, StyledP, StyledLi } from '../../theme/globalStyle'
 
 const Wrapper = styled.div`
   width: 400px;
   display: grid;
-  grid-template-columns: 3fr;
-  grid-template-rows: auto;
+  /* grid-template-columns: 3fr;
+  grid-template-rows: auto; */
   grid-template-areas: 'text text text';
-  background: ${props => props.theme.primary.light};
+  background: ${props => props.theme.white};
   border-radius: 4px;
+  border: 1px solid ${props => props.theme.primary.light};
+`
+
+const JobLink = styled(Link).attrs({
+  color: props => props.theme.secondary.red
+})`
+  color: inherit;
+  &:visited,
+  &:active {
+    color: inherit;
+  }
+  &:hover {
+    color: ${props => props.color};
+  }
+`
+const JobItem = StyledLi.extend`
+  display: inline;
+  text-transform: uppercase;
+  color: ${props =>
+    props['data-header']
+      ? props => props.theme.primary.light
+      : props => props.theme.white};
+  padding: 0.5rem;
 `
 
 const JobTitle = StyledH3.extend`
-  color: ${props => props.theme.white};
+  color: ${props => props.theme.primary.light};
   margin: 0.05rem;
   padding: 0rem;
 `
 
 const JobText = StyledP.extend`
-  color: ${props => props.theme.white};
+  color: ${props => props.theme.primary.light};
   margin: 0.05rem;
   padding: 0rem;
 `
 
 const Text = styled.div`
-  grid-area: text;
+  /* grid-area: text; */
   padding: 0.5rem;
   margin: 0.5rem;
 `
@@ -36,6 +60,7 @@ class JobsList extends React.Component {
     return (
       <Wrapper>
         <Text>
+          <JobLink>hello</JobLink>
           <JobTitle>{this.props.title}</JobTitle>
           <JobText>{this.props.text}</JobText>
         </Text>
