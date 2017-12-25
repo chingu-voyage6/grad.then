53cf1abb04dc6b0eee22766828beabe31af2a600
mar-bi <marina.biletska@gmail.com>
2017-12-25 09:41:44 +0200

    update jobs page

--- src/pages/jobs.js
+++ src/pages/jobs.js
@@ -1,29 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 import Hero from '../layouts/components/Hero'
 
 import { StyledH1, StyledP } from '../theme/globalStyle'
-
-import JobsList from '../layouts/components/JobsList'
-import Quote from '../layouts/components/Quote'
+import JobsContainer from '../layouts/components/JobsContainer'
 
 import faker from 'faker'
+
 
 const JobsPage = () => (
   <div>
     <Hero type="jobs" />
-    <StyledH1>Jobs Page</StyledH1>
-    <StyledP>This is the Jobs page</StyledP>
-    <Quote>Thin Jobs</Quote>
-    <JobsList
-      type={'list'}
-      title={faker.commerce.productName()}
-      text={faker.hacker.phrase()}
-    />
-    <Quote>Thick Jobs</Quote>
-    <JobsList
-      title={faker.commerce.productName()}
-      text={faker.hacker.phrase()}
-    />
+    <JobsContainer />
   </div>
 )
 
