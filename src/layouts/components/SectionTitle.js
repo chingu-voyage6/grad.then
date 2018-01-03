efe8abea7309fd7f55440015e63f269c46e4774e
mar-bi <marina.biletska@gmail.com>
2018-01-03 12:00:27 +0200

    add media queries to SectionTitle

--- src/layouts/components/SectionTitle.js
+++ src/layouts/components/SectionTitle.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import { StyledH2 } from '../../theme/globalStyle'
+import { StyledH2, media } from '../../theme/globalStyle'
+
 
 const SectionTitle = StyledH2.extend`
   text-align: center;
   text-transform: uppercase;
   color: ${props => props.theme.primary.light};
+  ${media.tablet`
+    font-size: 1.8rem;
+  `}
+  ${media.phone`
+    font-size: 1.46rem;
+  `}
 `
 export default SectionTitle
