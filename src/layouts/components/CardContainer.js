d8670551e953c09f99d8cd2ed636e0cf1ed1c3e5
mar-bi <marina.biletska@gmail.com>
2018-01-04 17:07:09 +0200

    responsive CardContainer

--- src/layouts/components/CardContainer.js
+++ src/layouts/components/CardContainer.js
@@ -2,31 +2,66 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
+import { media } from '../../theme/globalStyle'
+
 const StyledCardContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: auto;
   margin: 1.5rem 0 3rem 0;
+  ${media.desktop`
+    margin: 0;
+  `}
+  ${media.phone`
+    margin: 0.5rem 0 1rem 0;
+  `}
 `
 
 const StyledStories = styled.div`
-  grid-column: 3 / span 8;
+  grid-column: 2 / span 10;
   display: grid;
   grid-template-columns: ${props => `repeat(${props.cols}, 1fr)`};
   grid-template-rows: auto;
   grid-gap: 0.5rem;
   border: 2px solid ${props => props.theme.primary.dark};
-  padding: 1rem;
+  border-radius: 2px;
+  padding: 0.6rem;
+  & > div:last-child {
+    display: ${props => (props.cols < props.cards)? `none` : `flex`};
+  }
+  ${media.desktop`
+    grid-column: 1 / span 12;
+    margin: 0 1.5rem;
+  `}
+  ${media.tablet`
+    margin: 0.5rem 2rem;
+    grid-template-columns: ${props => (props.cols > 3)? `repeat(${Math.floor(props.cols / 2)}, 1fr)`: `repeat(${props.cols}, 1fr)`};
+    & > div:last-child {
+      display: flex;
+    }
+  `}
+  ${media.phone`
+    margin: 0.5rem;
+    grid-column: 2 / span 10;
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 1rem;
+  `}
 `
 
 const CardContainer = props => (
   <StyledCardContainer>
-    <StyledStories cols={props.cols}>{props.children}</StyledStories>
+    <StyledStories
+      cols={props.cols}
+      cards={props.cards}
+    >
+      {props.children}
+    </StyledStories>
   </StyledCardContainer>
 )
 
 CardContainer.propTypes = {
-  cols: PropTypes.number.isRequired,
+  cols: PropTypes.number.isRequired, //number of columns
+  cards: PropTypes.number, // number of cards
   children: PropTypes.array
 }
 
