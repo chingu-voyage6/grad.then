f9209c71ae726d9c43dd99429244d60e29a2aa9e
mar-bi <marina.biletska@gmail.com>
2018-01-10 16:29:30 +0200

    fix RegExp for 404Page

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,13 +2,11 @@
 exports.onCreatePage = ({ page, boundActionCreators }) => {
   const { createPage } = boundActionCreators
 
-  return new Promise((resolve, reject) => {
-    if (page.path.match(/^\/404/)) {
+  return new Promise((resolve) => {
+    if (page.path.match(/^\/404*/)) {
       page.layout = 'PageNotFound'
-
       createPage(page)
     }
-
     resolve()
   })
 }
