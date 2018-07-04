11e2dfdfc01fc003f1570208e841fe23dcaf4880
mar-bi <marina.biletska@gmail.com>
2018-07-04 10:52:52 +0200

    fix stylelint errors

---
+++ src/components/Quote.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+import { media } from '../theme/globalStyle'
+const QuoteText = styled.blockquote`
+  color: ${props => props.theme.primary.dark};
+  font-size: 1.5rem;
+  margin: 0.75rem 0 0;
+  font-style: italic;
+`
+
+const Separator = styled.hr`
+  height: 5px;
+  width: 40px;
+  background: ${props => props.theme.primary.dark};
+  margin-left: 0;
+  border: none;
+`
+
+const Wrapper = styled.div`
+  margin: 1rem 0;
+  ${media.giant`
+    margin: 0.8rem 0;
+  `} ${media.desktop`
+    margin: 0.6rem 0;
+  `} ${media.tablet`
+    margin: 0.4rem 0;
+  `};
+`
+
+const Quote = props => (
+  <Wrapper>
+    <Separator />
+    <QuoteText>{props.children}</QuoteText>
+  </Wrapper>
+)
+
+export default Quote
+
+Quote.propTypes = {
+  children: PropTypes.string
+}
--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -28,7 +28,7 @@ injectGlobal`
     line-height: 1.3125;
   }
 
-  p {
+  p, blockquote {
     font-family: OpenSans, Arial, sans-serif;
   }
 
@@ -42,12 +42,12 @@ injectGlobal`
     -moz-outline-style: none
   }
 
-  :focus {
-    outline:none;
-  }
-
   ::-moz-focus-inner {
     border:0;
+  }
+
+  :focus {
+    outline:none;
   }
 
   ul {
@@ -68,17 +68,20 @@ injectGlobal`
 `
 
 // iterate through the sizes and create a media template
-export const media = Object.keys(sizes).reduce((accumulator, label) => {
-  // use em in breakpoints to work properly cross-browser and support users
-  // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
-  const emSize = sizes[label] / 16
-  accumulator[label] = (...args) => css`
-    @media (max-width: ${emSize}em) {
-      ${css(...args)};
-    }
-  `
-  return accumulator
-}, {})
+export const media = Object.keys(sizes).reduce(
+  (accumulator, label) => {
+    // use em in breakpoints to work properly cross-browser and support users
+    // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
+    const emSize = sizes[label] / 16
+    accumulator[label] = (...args) => css`
+      @media (max-width: ${emSize}em) {
+        ${css(...args)};
+      }
+    `
+    return accumulator
+  },
+  {}
+)
 
 export const theme = {
   text: '#333',
