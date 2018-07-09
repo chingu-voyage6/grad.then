8e67406b86a6bf7ec0f6814c87a8386a752de5d6
mar-bi <marina.biletska@gmail.com>
2018-07-09 10:11:43 +0200

    add contentful blog query

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -3,21 +3,49 @@ import React from 'react'
 import Hero from '../components/Hero'
 import StoriesContainer from '../components/StoriesContainer'
 
-const StoriesPage = ({ data }) => (
-  <div>
-    <Hero type="stories" />
-    <StoriesContainer
-      menuFilter={data.site.siteMetadata.filterOptions.stories}
-    />
-  </div>
-)
+const StoriesPage = ({ data }) => {
+  const { site, allContentfulBlog: blog } = data
+  return (
+    <div>
+      <Hero type="stories" />
+      <StoriesContainer
+        menuFilter={site.siteMetadata.filterOptions.stories}
+        blog={blog.edges}
+      />
+    </div>
+  )
+}
 
 export const query = graphql`
-  query StroiesQuery {
+  query StoriesQuery {
     site {
       siteMetadata {
         filterOptions {
           stories
+        }
+      }
+    }
+    allContentfulBlog {
+      edges {
+        node {
+          id
+          title
+          slug
+          publishDate
+          author {
+            fullName
+            bio {
+              bio
+            }
+          }
+          content {
+            content
+          }
+          featureImage {
+            resolutions(width: 100, height: 100) {
+              src
+            }
+          }
         }
       }
     }
