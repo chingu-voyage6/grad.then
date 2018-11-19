e6c4ec52176e64f21c0f4e56efa3c2607297fd5d
spences10 <spences10apps@gmail.com>
2018-11-19 20:52:04 +0000

    gatsby link

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Link from 'gatsby-link'
+import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 
 import { StyledH3, StyledP, media } from '../theme/globalStyle'
